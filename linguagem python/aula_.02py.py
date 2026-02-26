88d534188aac3f05bc5cf37a4e3545083804fa0d
beatrizbazzarello-max <beatrizbazzarello@gmail.com>
2026-02-26 17:40:46 -0300

    Add files via upload

---
+++ linguagem python/aula_.02py.py
@@ -0,0 +1,22 @@
+# dados primitivos
+# em python
+
+# tipo string - texto
+'texto'
+" texto djfhhdhjjv"
+
+# tipo interger - inteiro
+
+1
+2
+100
+
+#  tipo float - real
+
+5.0 - 102.10
+100.
+0.5
+
+# tipo bool - logico
+True
+False
