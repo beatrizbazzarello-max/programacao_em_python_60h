52bfc8f49129fc401039c6870cc4cc48b03a27a0
beatrizbazzarello-max <beatrizbazzarello@gmail.com>
2026-02-27 17:23:27 -0300

    Add files via upload

---
+++ atividades_variaveis.py
@@ -0,0 +1,27 @@
+# atividade 1
+numero = int(input('digite um numero inteiro'))
+quadrado = numero **2 
+print('o quadrado desse numero é:', quadrado)
+
+
+# atividade 2
+nome = 'beatriz'
+sobrenome = 'lopes'
+
+print(nome,sobrenome)
+
+
+# atividade 3
+n1 = int(input('digite o primeiro numero inteiro'))
+n2 = int(input('digite o segundo numero inteiro'))
+print(n1,n2)
+
+# atividade 4
+palavra = 'python'
+numero = 8
+print(palavra, + numero)
+
+# atividade 5 
+variavel = 'alex gosta de jogar:'
+palavra = input('digite que alex gosta de jogar:')
+print(variavel, palavra)
