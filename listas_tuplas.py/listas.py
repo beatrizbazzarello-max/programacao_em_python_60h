21b9eeaa95821cdf6b37c5928340167c7904a5cf
beatrizbazzarello-max <beatrizbazzarello@gmail.com>
2026-03-06 14:45:36 -0300

    Add files via upload

---
+++ listas_tuplas.py/listas.py
@@ -0,0 +1,33 @@
+# atividade 0
+
+numero = list(range(2, 21, 2))
+print(numero)
+
+# atividade 1
+
+numeros = list[1,2,3,4,5,6,7,8,9,10]
+print(numeros)
+
+# atividade 2
+
+numeros = [1,2,3,4,5,6,7,8,9,10]
+print(numeros[3])
+
+# atividade 3
+
+numeros = [1,2,3,4,5,6,7,8,9,10]
+numeros.append(9)
+print(numeros)
+
+# atividade 4
+
+numeros = [1,2,3,4,5,6,7,8,9,10]
+numeros.remove(5)
+print(numeros)
+
+# atividade 5
+
+numeros = [1,2,3,4,5,6,7,8,9,10]
+carros = ['toyota', 'honda', 'ford']
+resultado = numeros + carros
+print(resultado)
