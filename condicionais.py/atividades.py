a41ccc2723b8722bd244ae700c9d82946b0783a3
beatrizbazzarello-max <beatrizbazzarello@gmail.com>
2026-03-11 17:58:37 -0300

    Add files via upload

---
+++ condicionais.py/atividades.py
@@ -0,0 +1,67 @@
+# atividade 1
+
+numero = float(input('digite um numero:'))
+
+if numero > 0:
+    print('numero positivo')
+
+elif numero < 0:
+    print('numero negativo')
+
+else:
+    print('numero zero')
+
+# atividade 2
+
+idade = int(input('digite sua idade:'))
+if idade >= 16:
+    print('pode votar!')
+
+else: 
+    print('não pode votar!')
+
+# atividade 3'
+
+numero = int(input('digite um numero:'))
+if numero % 2 == 0:
+    print('numero par')
+
+else:
+    print('numero impar ')
+
+# atividade 4
+
+a = float(input('digite o primeiro lado:'))
+b = float(input('digite o segundo lado:'))
+c = float(input('digite o terceiro lado:'))
+if a == b and b == c:
+    print('triangulo equilatero')
+
+elif a == b or a == c or b == c:
+    print('triangulo isosceles')
+else:
+    print('triangulo escaleno')
+
+# atividade 5
+
+numero = int(input('digite um numero:'))
+if numero % 5 == 0 and numero % 7 == 0:
+    print('é multiplo de 5 e 7')
+
+else:
+    print('não é multiplo de 5 e 7')
+
+# atividade 6
+
+numero = float(input('digite um numero:'))
+if numero > 0 and  numero > 10:
+    print('o numero é positivo ')
+
+else:
+    print('nao e maior que 10')
+
+# atividade 7
+
+n2 = int(input('digite um numero:'))
+if n2 % 3 == 0 and n2 % 5 == 0:
+
